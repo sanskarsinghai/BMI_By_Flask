0eb331e807d190d1e24a145ec3a434ab2e8b0395
sanskarsinghai <sanskar12singhai@gmail.com>
2023-05-13 19:03:50 +0530

    first

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request
+
+app =Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def bmical():
+    bmi=''
+    if request.method=="POST":
+        w=int(request.form['weight'])
+        h=float(request.form['height'])
+        bmi=str(w/(h*h))
+    return render_template('index.html',bmi=bmi)
+
+app.run(port=5000)
